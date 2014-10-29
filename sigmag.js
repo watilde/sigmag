8eaef88010df53c3ccd7f103c57ce04d08a82794
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-10-30 00:06:31 +0900

    Update demo

--- sigmag.js
+++ sigmag.js
@@ -1,25 +1,30 @@
-$.fn.sigma = function () {
+$.fn.sigma = function (debug) {
   'use strict';
   function sigmaToCalculation(i, u, l, m) {
     var calculation = '(function () {';
     calculation += 'var sum = 0;';
-    calculation += 'for (var ' + i + ' = ' + l + '; ' + u + ' >= ' + i + '; ' + i + '++) {';
+    calculation += 'for (var ' + i + ' = ' +
+      l + '; ' + u + ' >= ' + i + '; ' + i + '++) {';
     calculation += '  sum += ' + m + ';';
     calculation += '}';
     calculation += 'return sum;';
     calculation += '}());';
     return calculation;
   }
-
-  var index = $(this).children('.sigma__index').html();
-  var upper = $(this).children('.sigma__upper-bound').html();
-  var lower = $(this).children('.sigma__lower-bound').html();
-  var map = $(this).children('.sigma__map').html();
-  var calculation = sigmaToCalculation(index, upper, lower, map);
-  var result = eval(calculation);
-  var div = $("<div>");
-  $(div).addClass('sigma__result').html(result);
-  $(this).remove('.sigma__result').append(div);
+  try {
+    var index = $(this).children('.sigma__index').html();
+    var upper = $(this).children('.sigma__upper-bound').html();
+    var lower = $(this).children('.sigma__lower-bound').html();
+    var map = $(this).children('.sigma__map').html();
+    var calculation = sigmaToCalculation(index, upper, lower, map);
+    var result = eval(calculation);
+    var div = $("<div>");
+    $(div).addClass('sigma__result').html(result);
+    $(this).children('.sigma__result').remove();
+    $(this).append(div);
+  } catch (e) {
+    if (debug) console.error(e.message);
+  }
 };
 
 $(document).ready(function () {
